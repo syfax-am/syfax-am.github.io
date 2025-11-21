b868e7e6147d360570e44f89865446fa1f124936
syfax-am <syfaxaitmedjber@gmail.com>
2025-11-21 20:01:48 +0100

    final fix

--- src/App.tsx
+++ src/App.tsx
@@ -22,9 +22,8 @@ import {
   Layers,
   Globe
 } from 'lucide-react';
-import profileImg from './assets/id.jpg';
-import cvFile from './assets/CV_syfax-ait-medjber.pdf';
-// --- DATA    : PROJECTS ---
+
+// --- DATA: PROJECTS ---
 const projectsData = [
   {
     id: 1,
@@ -503,8 +502,8 @@ function HomePage() {
                 </a>
               </div>
               <a
-                href={cvFile}
-                download="CV_Syfax_Ait_Medjber.pdf" 
+                href="assets/CV_syfax-ait-medjber.pdf"
+                download
                 className="inline-flex items-center px-8 py-3 bg-[#476da3] hover:bg-[#375580] rounded-full text-white font-medium transition-all shadow-lg shadow-blue-900/25 hover:shadow-blue-900/40 hover:-translate-y-0.5"
               >
                 Download CV <Download size={18} className="ml-2" />
@@ -642,7 +641,7 @@ function HomePage() {
             >
               <div className="absolute -inset-4 bg-[#476da3] rounded-2xl opacity-20 blur-xl"></div>
               <img
-                src={profileImg}
+                src="https://media.licdn.com/dms/image/v2/D4D03AQFIvsn-6gzNmQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1731677331865?e=1765411200&v=beta&t=kRBov8BuuKqSR-gX7bIFd5qlW6TgbEDYtwoG53PW1OU"
                 alt="Syfax Ait Medjber"
                 className="relative rounded-xl object-cover w-full shadow-2xl border border-zinc-700/50 grayscale hover:grayscale-0 transition-all duration-500"
               />
